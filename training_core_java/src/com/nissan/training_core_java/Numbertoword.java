0f72c0ef1c51162622ded2e953c797f101155e22
Sankar <sankarsreekumar1996@gmail.com>
2018-10-30 16:24:12 +0530

    number to word

--- training_core_java/src/com/nissan/training_core_java/Numbertoword.java
+++ training_core_java/src/com/nissan/training_core_java/Numbertoword.java
@@ -15,13 +15,7 @@ public static String ntw(double x)
 	double x1=x-a1;
 	int x2=(int)(x1*100);
 	int i[]= {a1,x2};
-	int flag=0;
-	while(flag<2)
-	{
-	a=i[flag];
-	if(flag==1)
-		wrd=wrd+" and";
-	flag++;
+	a=a1;
 	 w1="";
 	 w2="";
 	int b=a/1000;
@@ -33,11 +27,11 @@ public static String ntw(double x)
 	int h=(a/100)%10;
 	if(c1==1)
 	{
-		wrd=wrd+" "+num1[c2];
+		wrd=wrd+num1[c2]+" ";
 	}
 	else
 	{
-		wrd=wrd+" "+num2[c1]+" "+num[c2];
+		wrd=wrd+num2[c1]+" "+num[c2]+" ";
 	}
 	if(a/100!=0)
     if(h!=0)
@@ -53,10 +47,23 @@ public static String ntw(double x)
 		wrd=num2[b1]+" "+num[b2]+" thousand "+wrd;
 	}
 	}
-	if(flag==1)
-	wrd="Rupees "+wrd;
-	else
-		wrd=wrd+" paisa";
+	int j=0;int l;
+	int z=x2;
+	if(x2>0)
+	{
+		wrd=wrd+"point ";
+	while(x2>0)
+	{
+		j++;
+		x2=x2/10;
+	}
+	while(j>0)
+	{
+		int digit=(int)(z/Math.pow(10,j-1));
+				wrd=wrd+num[digit]+" ";
+				z=(int)(z%Math.pow(10,j-1));
+				j--;
+	}
 	}
 	return wrd;
 	
@@ -64,13 +71,24 @@ public static String ntw(double x)
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the amount in digits");
+		System.out.println("Enter the number upto 2 decimal places");
 		double a=sc.nextDouble();
-		while(a>0)
+		while(a!=0)
 		{
+			if(a<0)
+		System.out.println("minus "+ntw(-a));
+			else
 		System.out.println(ntw(a));
-		System.out.println("Enter the amount in digits");
+		System.out.println("Enter the number(Enter zero to exit)");
+		try {
 		a=sc.nextDouble();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			System.out.println("Invalid input");
+			break;
+		}
 		}
 	}
 
